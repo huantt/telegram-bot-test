4483eca2b6f22b8bec8fdaef485a26836e341269
jack <tathuan96@gmail.com>
2024-04-24 10:04:34 +0700

    chore: allow to override port via env

--- api/init.go
+++ api/init.go
@@ -1,16 +1,21 @@
 package api
 
 import (
+	"fmt"
 	"github.com/huantt/telegram-bot-test/api/webhook"
 	"log"
 	"net/http"
+	"os"
 )
 
 func Init() {
 	webhookHandler := webhook.NewHandler(webhook.NewService())
 	http.HandleFunc("/webhook/updates", webhookHandler.Handle)
-
-	err := http.ListenAndServe(":9000", nil)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "9000"
+	}
+	err := http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
 	if err != nil {
 		log.Fatalf("%+v", err)
 	}
