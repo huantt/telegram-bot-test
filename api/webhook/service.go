582991f1162e8e4cb9cf9caa4927dec2d5562a52
jack <tathuan96@gmail.com>
2024-04-24 12:10:28 +0700

    chore: respond

--- api/webhook/service.go
+++ api/webhook/service.go
@@ -19,7 +19,9 @@ func NewService(bot *tgbotapi.BotAPI) *Service {
 func (s *Service) Handle(ctx context.Context, update *tgbotapi.Update) error {
 	if update != nil && update.Message != nil {
 		slog.Info(fmt.Sprintf("Latency: %s", time.Since(update.Message.Time())))
+		start := time.Now()
 		_, err := s.bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, "Hello"))
+		slog.Info(fmt.Sprintf("Send latency: %s", time.Since(start)))
 		return err
 	}
 	return nil
